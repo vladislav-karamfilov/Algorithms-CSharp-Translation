c8d1c5f200fcd02a390405fc5dc81a04e766d54a
Vladislav Karamfilov <Vladislav.Karamfilov@gmail.com>
2013-07-29 23:56:17 +0300

    Chapter 1 translated to Pascal Triangle

--- Programming =++ Algorithms C#/Chapter 1/IntroductionToAlgorithms/FillingAndPrintingMatrix/FillingAndPrintingMatrix.cs
+++ Programming =++ Algorithms C#/Chapter 1/IntroductionToAlgorithms/FillingAndPrintingMatrix/FillingAndPrintingMatrix.cs
@@ -2,34 +2,34 @@
 
 class FillingAndPrintingMatrix
 {
-    static int[,] A = new int[5, 4];
-    static int[,] B = new int[5, 4];
-
     static void Main()
     {
-        FillMatrixByRows();
-        FillMatrixByColumns();
+        int[,] A = new int[5, 4];
+        FillMatrixByRows(A);
         PrintMatrix(A);
+
+        int[,] B = new int[5, 4];
+        FillMatrixByColumns(B);
         PrintMatrix(B);
     }
 
-    static void FillMatrixByRows()
+    static void FillMatrixByRows(int[,] matrix)
     {
-        for (int row = 0; row < A.GetLength(0); row++)
-            for (int col = 0; col < A.GetLength(1); col++)
+        for (int row = 0; row < matrix.GetLength(0); row++)
+            for (int col = 0; col < matrix.GetLength(1); col++)
             {
                 Console.Write("Въведи число на ред {0} колона {1}: ", row, col);
-                A[row, col] = int.Parse(Console.ReadLine());
+                matrix[row, col] = int.Parse(Console.ReadLine());
             }
     }
 
-    static void FillMatrixByColumns()
+    static void FillMatrixByColumns(int[,] matrix)
     {
-        for (int col = 0; col < A.GetLength(1); col++)
-            for (int row = 0; row < A.GetLength(0); row++)
+        for (int col = 0; col < matrix.GetLength(1); col++)
+            for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 Console.Write("Въведи число на ред {0} колона {1}: ", row, col);
-                A[row, col] = int.Parse(Console.ReadLine());
+                matrix[row, col] = int.Parse(Console.ReadLine());
             }
     }
 
