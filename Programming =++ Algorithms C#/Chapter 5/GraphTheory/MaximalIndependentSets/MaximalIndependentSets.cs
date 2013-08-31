d3c7c1e77bab47e014a55ea84b5682188705cd59
Vladislav Karamfilov <Vladislav.Karamfilov@gmail.com>
2013-08-31 12:11:32 +0300

    Translated to max independent sets

---
+++ Programming =++ Algorithms C#/Chapter 5/GraphTheory/MaximalIndependentSets/MaximalIndependentSets.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MaximalIndependentSets
+{
+    const int VerticesCount = 8;
+    static readonly byte[,] Graph = new byte[VerticesCount, VerticesCount]
+    {
+        { 0, 1, 0, 0, 0, 1, 0, 1 },
+        { 1, 0, 1, 0, 0, 1, 0, 0 },
+        { 0, 1, 0, 1, 1, 0, 0, 0 },
+        { 0, 0, 1, 0, 1, 0, 1, 0 },
+        { 0, 0, 1, 1, 0, 1, 0, 0 },
+        { 1, 1, 0, 0, 1, 0, 1, 1 },
+        { 0, 0, 0, 1, 0, 1, 0, 0 },
+        { 1, 0, 0, 0, 0, 1, 0, 0 }
+    };
+    
+
+    static void Main()
+    {
+        Console.WriteLine("Всички максимални независими множества в графа са:");
+        FindMaxIndependentSets(0);
+    }
+
+    static void FindMaxIndependentSets(int last)
+    {
+
+    }
+
+
+
+    static void PrintSet()
+    {
+        
+    }
+}
