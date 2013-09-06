caacb56d88b360c4c71bac5c94ec72b0074f830a
staafl <velko.nikolov@gmail.com>
2013-09-06 14:10:51 +0300

    fixed matrix2.cs and railway.cs

--- Programming =++ Algorithms C#/Chapter 8/matrix2.cs
+++ Programming =++ Algorithms C#/Chapter 8/matrix2.cs
@@ -1,20 +1,21 @@
 using System;
 class Program
-{ const int NOT_SOLVED = -1;
+{ const long NOT_SOLVED = long.MaxValue;
   const int n = 9;         /* Брой матрици */
-  long m[n,n];    /* Таблица - целева функция */
-  static long r[n+1] = {12,13,35,3,34,2,21,10,21,6}; /* Размерности на матриците */
+  static long[,] m = new long[n+1,n+1];    /* Таблица - целева функция */
+  static long[] r = new long[]{12,13,35,3,34,2,21,10,21,6}; /* Размерности на матриците */
   
   static long SolveMemo(int i, int j)
-  { if (NOT_SOLVED != m[i,j]) /* Стойността вече е била пресметната */
+  { if (m[i,j] != NOT_SOLVED) /* Стойността вече е била пресметната */
       return m[i,j];
     if (i == j)                /* В този интервал няма матрица */
       m[i,j] = 0;
     else {                     /* Пресмятаме рекурсивно */
-      for (int k = i; k <= j - 1; k++)
-        long q = solveMemo(i, k) + solveMemo(k + 1, j) + r[i-1] * r[k] * r[j];
+      for (int k = i; k <= j - 1; k++) {
+        long q = SolveMemo(i, k) + SolveMemo(k + 1, j) + r[i-1] * r[k] * r[j];
         if (q < m[i,j])
   		m[i,j] = q;
+  	}
     }
     return m[i,j];
   }
@@ -27,7 +28,7 @@ class Program
   }
   
   static void PrintMatrix() /* Извежда матрицата на минимумите на екрана */
-  { Console.Write("Матрица на минимумите:");
+  { Console.WriteLine("Матрица на минимумите:");
     for (int i = 1; i <= n; i++) {
       for (int j = 1; j <= n; j++)
         Console.Write("{0,8}", m[i,j]);
@@ -36,8 +37,7 @@ class Program
   }
   
   static void Main() {
-    Console.Write("Минималният брой умножения е: {0}", SolveMemoization());
+    Console.WriteLine("Минималният брой умножения е: {0}", SolveMemoization());
     PrintMatrix();
   }
-
 }
--- Programming =++ Algorithms C#/Chapter 8/railway.cs
+++ Programming =++ Algorithms C#/Chapter 8/railway.cs
@@ -1,43 +1,45 @@
 using System;
 class Program
-{ long minPrice[n];  /* Минимална цена на билета от началната до текущата гара */
+{ const long NOT_CALCULATED = long.MaxValue;
+  static long[] minPrice = new long[n];  /* Минимална цена на билета от началната до текущата гара */
   
   static long[] dist = {0,3,7,8,13,15,23}; /* Разстояние от началната гара */
   const long l1 = 3,  l2 = 6,  l3 = 8,
                       c1 = 20, c2 = 30, c3 = 40;
   const int n = 7;
   const int end = 6;
-  int start = 2;
+  static int start = 2;
   
   static long Calc(int cur)
-  { int i;
-    long price;
-    if (minPrice[cur] == NOT_CALCULATED ) {
+  { long price;
+    int i;
+    if (minPrice[cur] == NOT_CALCULATED) {
       /* Търсим най-лявата гара и пресмятаме евентуалната цена, ако вземем билет тип 1 */
-      for (int i = cur - 1; i >= start && (dist[cur] - dist[i]) <= l1; i--);
+      for (i = cur - 1; i >= start && (dist[cur] - dist[i]) <= l1; i--);
       if (++i < cur)
-        if ((price = calc(i) + c1) < minPrice[cur])
+        if ((price = Calc(i) + c1) < minPrice[cur])
   		 minPrice[cur] = price;
       /* Търсим най-лявата гара и пресмятаме евентуалната цена, ако вземем билет тип 2 */
       for (; i >= start && (dist[cur] - dist[i]) <= l2; i--);
       if (++i < cur)
-        if ((price = calc(i) + c2) < minPrice[cur]) minPrice[cur] = price;
+        if ((price = Calc(i) + c2) < minPrice[cur]) minPrice[cur] = price;
       /* Търсим най-лявата гара и пресмятаме евентуалната цена, ако вземем билет тип 3 */
       for (; i >= start && (dist[cur] - dist[i]) <= l3; i--);
       if (++i < cur)
-        if ((price = calc(i) + c3) < minPrice[cur]) minPrice[cur] = price;
+        if ((price = Calc(i) + c3) < minPrice[cur]) minPrice[cur] = price;
     }
     return minPrice[cur];
   }
   
   static void Main() {
     /* Иницализация */
-    for (int i = 0; i < start; i++)
+    int i;
+    for (i = 0; i < start; i++)
   	minPrice[i] = 0;
     for (; i < n; i++)
   	minPrice[i] = NOT_CALCULATED;
     /* Решаване на задачата */
     start--;
-    Console.WriteLine("Минимална цена: {0}", calc(end-1));
+    Console.WriteLine("Минимална цена: {0}", Calc(end-1));
   }
 }
