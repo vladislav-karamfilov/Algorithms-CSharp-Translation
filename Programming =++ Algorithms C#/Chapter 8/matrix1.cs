c57a77c6b61ef17d1f91f83b6a3475fa058096e5
nobody <none@none.com>
2013-09-06 08:22:07 +0300

    chapter 8 lcs1.c to triangle.c raw

---
+++ Programming =++ Algorithms C#/Chapter 8/matrix1.cs
@@ -0,0 +1,36 @@
+﻿
+static long[,] m = new long[10,10];    /* Таблица - целева функция */
+
+static long[] r = new long[]{12,13,35,3,34,2,21,10,21,6}; /* Размерности на матриците */
+const int n = 9;         /* Брой матрици */
+
+/* Неефективна рекурсивна функция */
+static long SolveRecursive(int i, int j)
+{ if (i == j) return 0;
+  m[i,j] = int.MaxValue;
+  for (int k = i; k <= j - 1; k++) {
+    long q = SolveRecursive(i, k) +
+      SolveRecursive(k + 1, j) +
+      r[i - 1] *
+      r[k] *
+      r[j];
+    if (q < m[i,j])
+	  m[i,j] = q;
+  }
+
+  return m[i,j];
+}
+
+static void PrintMatrix() /* Извежда матрицата на минимумите на екрана */
+{ Console.Write("Матрица на минимумите:");
+  for (int i = 1; i <= n; i++) {
+    Console.Write("\n");
+    for (int j = 1; j <= n; j++)
+      Console.Write("{0,8}", m[i,j]);
+  }
+}
+
+static void Main() {
+  Console.WriteLine("Минималният брой умножения е: {0}",SolveRecursive(1,n));
+  PrintMatrix();
+}
---
+++ Programming =++ Algorithms C#/Chapter 8/matrix2.cs
@@ -0,0 +1,39 @@
+﻿const int NOT_SOLVED = -1;
+const int n = 9;         /* Брой матрици */
+long m[n,n];    /* Таблица - целева функция */
+static long r[n+1] = {12,13,35,3,34,2,21,10,21,6}; /* Размерности на матриците */
+
+static long SolveMemo(int i, int j)
+{ if (NOT_SOLVED != m[i,j]) /* Стойността вече е била пресметната */
+    return m[i,j];
+  if (i == j)                /* В този интервал няма матрица */
+    m[i,j] = 0;
+  else {                     /* Пресмятаме рекурсивно */
+    for (int k = i; k <= j - 1; k++)
+      long q = solveMemo(i, k) + solveMemo(k + 1, j) + r[i-1] * r[k] * r[j];
+      if (q < m[i,j])
+		m[i,j] = q;
+  }
+  return m[i,j];
+}
+
+static long SolveMemoization()
+{ for (int i = 1; i <= n; i++)
+    for (int j = i; j <= n; j++)
+	  m[i,j] = NOT_SOLVED;
+  return SolveMemo(1, n);
+}
+
+static void PrintMatrix() /* Извежда матрицата на минимумите на екрана */
+{ Console.Write("Матрица на минимумите:");
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++)
+      Console.Write("{0,8}", m[i,j]);
+    Console.WriteLine();
+  }
+}
+
+static void Main() {
+  Console.Write("Минималният брой умножения е: {0}", SolveMemoization());
+  PrintMatrix();
+}
